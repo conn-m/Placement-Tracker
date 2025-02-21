0baf2bb02c70383f2ec8f4bea5f617a3f9a882dc
Tony Griffiths <tony_griffiths_66@hotmail.com>
2025-02-21 15:35:38 +0000

    Set Login as start up page

--- PlacementTracker.Web/Program.cs
+++ PlacementTracker.Web/Program.cs
@@ -54,7 +54,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Login}/{id?}");
 
 
 
